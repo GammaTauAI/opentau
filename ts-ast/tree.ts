3f8e7a189cb2fdaaa6e219551d35cd904b2a8d93
elleven11 <federico.cassano@federico.codes>
2022-09-18 15:17:45 -0400

    wooo! doing something right

--- ts-ast/tree.ts
+++ ts-ast/tree.ts
@@ -19,28 +19,32 @@ export const makeTree = (sourceFile: ts.SourceFile): CodeBlockTree[] => {
     ctxName: string,
     ctxNode: CodeBlockTree | undefined
   ): CodeBlockTree => {
-    // only make children out of function declarations
     const code = printer.printNode(ts.EmitHint.Unspecified, child, sourceFile);
 
+    // only make children out of function declarations
     if (child.kind === ts.SyntaxKind.FunctionDeclaration) {
       const func = child as ts.FunctionDeclaration;
-      console.log(func.name);
       const name = func.name?.escapedText.toString() ?? "anonymous"; // TODO: handle this case, we don't want to do anon
       console.log("func name: ", name);
 
-      let ctxNode = { name, code, children: [] };
+      let thisNode = { name, code, children: [] };
 
-      func.body?.statements.forEach((child) => traverse(child, name, ctxNode));
+      func.body?.statements.forEach((child) => traverse(child, name, thisNode));
 
-      return ctxNode;
+      if (ctxNode) {
+        ctxNode.children.push(thisNode);
+      }
+
+      return thisNode;
     }
 
     if (ctxNode === undefined) {
-      return { name: "ctx", code, children: [] };
+      return { name: ctxName, code, children: [] }; // TODO: traverse children
     }
 
-    child.forEachChild((child2) => {
-      traverse(child2, ctxName, ctxNode);
+    child.forEachChild((child) => {
+      const node = traverse(child, ctxName, undefined);
+      // ctxNode.children.push(node);
     });
 
     return ctxNode;
