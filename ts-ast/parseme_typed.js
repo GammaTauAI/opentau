d888fa39aa27679fe8733f2d5b0a05afab601367
elleven11 <federico.cassano@federico.codes>
2022-09-18 13:25:14 -0400

    del test files in repo root

--- ts-ast/parseme_typed.js
+++
@@ -1,8 +0,0 @@
-function hello(name1, name2) {
-    function inner() {
-        var myStuff = "Hello, ";
-        return myStuff;
-    }
-    return inner() + name1 + ", " + inner() + name2;
-}
-console.log(hello("John", "Smith"));
--- ts-ast/parseme_typed.ts
+++
@@ -1,9 +0,0 @@
-function hello(name1: string, name2: string): string {
-  function inner() {
-    let myStuff = "Hello, ";
-    return myStuff;
-  }
-  return inner() + name1 + ", " + inner() + name2;
-}
-
-console.log(hello("John", "Smith"));
