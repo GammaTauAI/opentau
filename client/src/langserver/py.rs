9693e880d11f0f88b6b191a05ab6f7ca385fdc92
Noah Shinn <93103423+noahshinn024@users.noreply.github.com>
2022-10-10 19:49:09 -0400

    py uds init

--- client/src/langserver/py.rs
+++ client/src/langserver/py.rs
@@ -16,23 +16,85 @@ pub struct PyServer {
 
 #[async_trait]
 impl LangServer for PyServer {
-    async fn make(_path: &str) -> Result<Self, LangServerError>
-    where
-        Self: std::marker::Sized,
-    {
-        todo!("make py server. implement now")
+    async fn make(_path: &str) -> Result<Self, LangServerError> {
+        let pid = std::process::id();
+        let tmp_dir = std::env::temp_dir();
+        let tmp_socket_file = tmp_dir.join(format!("codex-{}.sock", pid));
+        println!("tmp_socket_file: {:?}", tmp_socket_file);
+
+        todo!("python server call");
+        let mut process = match tokio::process::Command::new("python")
+            .args([
+            ])
+            .stdout(Stdio::piped())
+            // stderr is open by default, we want to see the output
+            .spawn()
+        {
+            Ok(p) => p,
+            Err(_) => return Err(LangServerError::ProcessSpawn),
+        };
+
+        // before allowing to connect, wait for the process to output "Listening"
+        {
+            let stdout = process.stdout.as_mut().unwrap();
+            let reader = tokio::io::BufReader::new(stdout);
+            let mut lines = reader.lines();
+            println!("client output:");
+            while let Some(line) = lines.next_line().await.unwrap() {
+                println!("{}", line);
+                if line.contains("Listening") {
+                    break;
+                }
+            }
+        }
+        println!("client ready to connect to socket!");
+        let socket_path = tmp_socket_file.to_str().unwrap().to_string();
+        let socket = SocketAbstraction {
+            socket_path,
+            process,
+        };
+        Ok(Self { socket })
     }
 
-    async fn pretty_print(&self, _code: &str, _type_name: &str) -> Result<String, LangServerError> {
-        todo!("pretty print py server. implement now")
+    async fn pretty_print(&self, code: &str, type_name: &str) -> Result<String, LangServerError> {
+        let req = LSPrintReq {
+            cmd: "print".to_string(),
+            text: base64::encode(code),
+            type_name: type_name.to_string(),
+        };
+
+        let resp = self.socket.send_req(&req).await?;
+        // decode the response
+        let resp = base64::decode(resp["text"].as_str().unwrap()).unwrap();
+
+        Ok(String::from_utf8(resp).unwrap())
     }
 
-    async fn to_tree(&self, _code: &str) -> Result<CodeBlockTree, LangServerError> {
-        todo!("tree py server. implement later")
+    async fn to_tree(&self, code: &str) -> Result<CodeBlockTree, LangServerError> {
+        let req = LSReq {
+            cmd: "tree".to_string(),
+            text: base64::encode(code),
+        };
+
+        let resp = self.socket.send_req(&req).await?;
+
+        // decode the response
+        let tree = base64::decode(resp["text"].as_str().unwrap()).unwrap();
+
+        Ok(serde_json::from_slice(&tree).unwrap())
     }
 
     async fn stub(&self, _code: &str) -> Result<String, LangServerError> {
-        todo!("stub py server. implement later")
+        let req = LSReq {
+            cmd: "stub".to_string(),
+            text: base64::encode(code),
+        };
+
+        let resp = self.socket.send_req(&req).await?;
+        // decode the response
+        let resp = base64::decode(resp["text"].as_str().unwrap()).unwrap();
+
+        Ok(String::from_utf8(resp).unwrap())
     }
 
     async fn check_complete(
@@ -40,10 +102,18 @@ impl LangServer for PyServer {
         _original: &str,
         _completed: &str,
     ) -> Result<(bool, i64), LangServerError> {
-        // TODO: implement this later, for now just return true
-        //       this needs to be implemented before tree and stub, but after make, print and
-        //       type_check
-        Ok((true, 0))
+        // encode original and completed into json: {original: "", completed: ""}
+        let req = LSCheckReq {
+            cmd: "check".to_string(),
+            text: base64::encode(completed),
+            original: base64::encode(original),
+        };
+
+        let resp = self.socket.send_req(&req).await?;
+        Ok((
+            resp["text"].as_bool().unwrap(),
+            resp["score"].as_i64().unwrap(),
+        ))
     }
 
     async fn type_check(&self, code: &str) -> Result<bool, LangServerError> {
---
+++ py-ast/main.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import time
+import json
+import sched
+import base64
+import socket
+
+
+if len(sys.argv) != 4:
+    print(f'usage: [path to socket] [pid of rust proc]')
+    sys.exit(1)
+
+SERVER_ADDR = sys.argv[2]
+
+try:
+    os.unlink(SERVER_ADDR)
+except OSError:
+    if os.path.exists(SERVER_ADDR):
+        print(f'{SERVER_ADDR} already exists')
+        sys.exit(1)
+
+# periodically check rust proc
+rust_pid = int(sys.argv[3])
+
+s = sched.scheduler(time.time, time.sleep)
+
+def run_func(sc):
+    is_pid_running(rust_pid)
+    sc.enter(3, 1, run_func, (sc,))
+s.enter(3, 1, run_func, (s,))
+s.run()
+
+def is_pid_running(pid: int) -> bool:
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    else:
+        return True
+
+def init_wait(s: socket.socket) -> None:
+    while True:
+        c, client_addr = s.accept()
+        try:
+            while True:
+                data = c.recv(64)
+                obj = json.loads(data) # FIXME: try catch
+                decoded_text = base64.b64decode(obj.text)
+                if obj.cmd == 'print':
+                    # TODO: send print
+                    NotImplemented()
+                elif obj.cmd == 'tree':
+                    # TODO: gen tree
+                    NotImplemented()
+                elif obj.cmd == 'stub':
+                    # TODO: gen stub
+                    NotImplemented()
+                elif obj.cmd == 'check':
+                    # TODO: check completion
+                    NotImplemented()
+                else:
+                    c.send(json.dumps({
+                        'type': 'error',
+                        'message': f'unknown command {obj.cmd}'
+                    }).encode())
+
+        finally:
+            c.close()
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+sock.bind(SERVER_ADDR)
+sock.listen(1)
+init_wait(sock)
