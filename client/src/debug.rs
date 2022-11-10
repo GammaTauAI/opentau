91ca97b75e294572f074dd60128168950cba2709
elleven11 <federico.cassano@federico.codes>
2022-11-09 19:38:32 -0500

    tree algo!

--- client/src/debug.rs
+++ client/src/debug.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use codex_types::{
     codex::CodexClientBuilder,
     langserver::{ts::TsServer, LangServer},
-    tree::{NaiveCompletionLevels, TreeCompletion},
+    tree::{CompletionLevels, NaiveCompletionLevels, TreeCompletion},
 };
 
 #[tokio::main]
@@ -44,15 +44,32 @@ async fn main() {
     // }
     // }
 
-    // testing out "tree"
+    // testing out "naive tree"
+    // {
+    // let tree = codex.get_ls().to_tree(&input).await.unwrap();
+    // let mut naive: NaiveCompletionLevels = NaiveCompletionLevels::prepare(tree, codex.get_ls())
+    // .await
+    // .unwrap();
+    // println!("tree: {:#?}", naive);
+    // let codex = codex.clone();
+    // naive.tree_complete(codex).await;
+    // println!("root comp:\n {}", naive.levels[0].nodes[0].code);
+    // }
+
+    // testing out "tree v2"
     {
         let tree = codex.get_ls().to_tree(&input).await.unwrap();
-        let mut naive: NaiveCompletionLevels = NaiveCompletionLevels::prepare(tree, codex.get_ls())
+        let mut comps: CompletionLevels = CompletionLevels::prepare(tree, codex.get_ls())
             .await
             .unwrap();
-        println!("tree: {:#?}", naive);
-        naive.tree_complete(codex).await;
-        println!("root comp:\n {}", naive.levels[0].nodes[0].code);
+        comps.retries = 1;
+        comps.num_comps = 4;
+        println!("tree: {:#?}", comps);
+        comps.tree_complete(codex).await;
+        println!("root comps:\n");
+        for (i, comp) in comps.levels[0].nodes[0].completed.iter().enumerate() {
+            println!("{}:\n{}", i, comp);
+        }
     }
 
     // testing out "stub"
--- client/src/tree.rs
+++ client/src/tree.rs
@@ -1,4 +1,8 @@
-use std::{cmp::Ordering, collections::HashMap, sync::Arc};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
 
 use serde::{Deserialize, Serialize};
 use tokio::task::JoinHandle;
@@ -31,21 +35,20 @@ pub trait TreeCompletion {
     async fn tree_complete(&mut self, codex: CodexClient);
 }
 
-// NOTE: tree compeltion algo (naive and inefficient way, with only one single completion per code-block):
-// - Step 1: make the code block tree out of the original input code.
-// - Step 2: make a nested array of code blocks, where the outer array index is the
-//   level of the blocks at the array of that index. every element of the inner arrays is a tuple
-//   of (children_idxs: Vec<usize>, name: String, code: String, usages: String).
-// - Step 3: we start at the deepest level of the array, and we complete the code blocks
-//   at the level.
-// - Step 4: we then go to a level above, and for each node, substitute the stub of the code block
-//   blocks bellow of the children. we then complete the code blocks at the level.
-// - Step 5: we repeat step 4 until we reach the root level. we have a completed code block tree.
-// - Step 6: we disassemble the code block tree and substitute the completed types into the original
-//   code. we type check the code, and if it passes, we return the code. if it fails, we go to step 3.
-//
-// Why the nested array instead of just using DFS? This representation allows to complete concurrently
-
+/// tree compeltion algo (naive and inefficient way, with only one single completion per code-block):
+/// - Step 1: make the code block tree out of the original input code.
+/// - Step 2: make a nested array of code blocks, where the outer array index is the
+///   level of the blocks at the array of that index. every element of the inner arrays is a tuple
+///   of (children_idxs: Vec<usize>, name: String, code: String, usages: String).
+/// - Step 3: we start at the deepest level of the array, and we complete the code blocks
+///   at the level.
+/// - Step 4: we then go to a level above, and for each node, substitute the stub of the code block
+///   blocks bellow of the children. we then complete the code blocks at the level.
+/// - Step 5: we repeat step 4 until we reach the root level. we have a completed code block tree.
+/// - Step 6: we disassemble the code block tree and substitute the completed types into the original
+///   code. we type check the code, and if it passes, we return the code. if it fails, we go to step 3.
+///
+/// Why the nested array instead of just using DFS? This representation allows to complete concurrently
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct NaiveNode {
     pub children_idxs: Vec<usize>,
@@ -213,15 +216,215 @@ impl TreeCompletion for NaiveCompletionLevels {
     }
 }
 
-// NOTE: tree compeltion algo v2 (more expensive but more accurate):
-// - Step 1: make the code block tree out of the original input code.
-// - Step 2: make a nested array of code blocks, where the outer array index is the
-//   level of the blocks at the array of that index. every element of the inner arrays is a tuple
-//   of (children_idxs: Vec<usize>, name: String, code: Vec<String>).
-// - Step 3: we start at the deepest level of the array, and we complete the code blocks
-//   at the level.
-// - Step 4: we then go to a level above, and for each node, substitute the stub of the code block
-//   blocks bellow of the children for each code in the vec. we then complete the code blocks at the level.
-// - Step 5: we repeat step 4 until we reach the root level. we have a completed code block tree.
-// - Step 6: we disassemble the code block tree and substitute the completed types into the original
-//   code. we type check the code, and if it passes, we return the code. if it fails, we go to step 3.
+/// tree compeltion algo v2 (more expensive but more accurate):
+/// - Step 1: make the code block tree out of the original input code.
+/// - Step 2: make a nested array of code blocks, where the outer array index is the
+///   level of the blocks at the array of that index. every element of the inner arrays is a tuple
+///   of (children_idxs: Vec<usize>, name: String, code: String, completed: Vec<String>).
+///   where `code` is the original code of the code block.
+/// - Step 3: we start at the deepest level of the array, and we complete the code blocks
+///   at the level.
+/// - Step 4: we then go to a level above, and for each node, substitute the stub of the code block
+///   blocks bellow of the children for each code in the vec. we then complete the code blocks at the level.
+/// - Step 5: we repeat step 4 until we reach the root level. we have a completed code block tree.
+/// - Step 6: we disassemble the code block tree and substitute the completed types into the original
+///   code. we type check the code, and if it passes, we return the code. if it fails, we go to step 3.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CompNode {
+    pub children_idxs: Vec<usize>,
+    pub name: String,
+    pub code: String,
+    pub completed: Vec<String>,
+    pub usages: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CompLevel {
+    pub nodes: Vec<CompNode>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct CompletionLevels {
+    pub levels: Vec<CompLevel>,
+    // the comp levels have variable retries and num_comps for codex
+    pub retries: usize,
+    pub num_comps: usize,
+}
+
+#[async_trait::async_trait]
+impl TreeCompletion for CompletionLevels {
+    /// Returns a tree completion for the given codeblock tree
+    async fn prepare(
+        tree: CodeBlockTree,
+        langsever: ArcLangServer,
+    ) -> Result<Self, LangServerError> {
+        // dyanmic programming solution. took me a while to figure out how to do this.
+
+        // here we have the levels of the tree
+        let mut levels = vec![];
+        // this is a memoization table, where we store the nodes of the tree at each level,
+        // with the children idx that need to be patched in the level after
+        let mut nodes = vec![CompNode {
+            children_idxs: vec![],
+            name: tree.name,
+            code: tree.code,
+            completed: vec![],
+            usages: String::new(), // no usages for root..
+        }];
+        // here we store the children of the nodes, and the idx of the node that they belong to
+        let mut p_children = vec![(0, tree.children)];
+
+        while !nodes.is_empty() {
+            // we push the level of nodes that we got the iteration before
+            levels.push(CompLevel { nodes });
+            // new nodes!
+            nodes = vec![];
+
+            // new children memoization, that we build up in this iteration
+            let mut new_children = vec![];
+            // for each node
+            for (p_idx, children) in p_children {
+                // for each children
+                for child in children {
+                    // the idx of the level that we will be patching in the next iteration
+                    let idx = nodes.len();
+
+                    // we patch the children idxs of the parent of the children
+                    let level = levels.len() - 1;
+                    let parent = levels.get_mut(level).unwrap().nodes.get_mut(p_idx).unwrap();
+                    parent.children_idxs.push(idx);
+
+                    // we get the usages of this node, from the parent
+                    let usages = langsever.usages(&parent.code, &child.code).await?;
+
+                    // push unpatched node
+                    nodes.push(CompNode {
+                        children_idxs: vec![],
+                        name: child.name,
+                        code: child.code,
+                        completed: vec![],
+                        usages,
+                    });
+                    // push children
+                    new_children.push((idx, child.children));
+                }
+            }
+            // reassign children
+            p_children = new_children;
+        }
+        Ok(CompletionLevels {
+            levels,
+            retries: 1,
+            num_comps: 1,
+        })
+    }
+
+    /// Completes the code block tree, mutating the tree in place.
+    async fn tree_complete(&mut self, codex: CodexClient) {
+        async fn retry_query_until_ok(codex: &CodexClient, q: CompletionQuery) -> Vec<Completion> {
+            let mut res = codex.complete(q.clone()).await;
+            let mut retries = 0;
+            while res.is_err() {
+                if retries > 5 {
+                    eprintln!("Failed to complete query: {}", q.input);
+                    std::process::exit(1);
+                }
+                retries += 1;
+                let q = q.clone();
+                res = codex.complete(q).await;
+            }
+            res.unwrap()
+        }
+
+        // we start at the deepest level of the array, and we complete the code blocks
+        // at the level.
+        let num_levels = self.levels.len();
+        let mut prev_level: Arc<Option<Vec<CompNode>>> = Arc::new(None);
+        for level in (0..num_levels).rev() {
+            debug!("level: {}", level);
+            let nodes = &mut self.levels.get_mut(level).unwrap().nodes;
+            let mut handles: Vec<JoinHandle<(String, Vec<String>)>> = vec![]; // node's (name, code)
+            let mut lookup: HashMap<String, usize> = HashMap::new(); // node's name -> idx
+            for (i, node) in nodes.iter().enumerate() {
+                let node = node.clone();
+                let prev_level = prev_level.clone();
+                let num_comps = self.num_comps;
+                let retries = self.retries;
+                let codex = codex.clone();
+                lookup.insert(node.name.clone(), i);
+                // we concurrently complete the code blocks at the level.
+                handles.push(tokio::task::spawn(async move {
+                    let mut prompts: Vec<String> = vec![node.code.clone()];
+                    // if we are not at a leaf, we need to patch the node with the children
+                    if !node.children_idxs.is_empty() {
+                        let level_below: &Vec<CompNode> = prev_level.as_ref().as_ref().unwrap();
+                        for child_idx in node.children_idxs.iter() {
+                            let child = level_below.get(*child_idx).unwrap_or_else(|| {
+                                panic!(
+                                    "child_idx {} should be in level_below, which has len {}",
+                                    child_idx,
+                                    level_below.len()
+                                )
+                            });
+                            // make all possible permutations between code elements and
+                            // child.completed elements
+                            let mut new_prompts = vec![];
+                            for parent_code in prompts.iter() {
+                                for child_code in child.completed.iter() {
+                                    let comp = codex
+                                        .get_ls()
+                                        // we take the min because at level 0 we have the root node
+                                        // and we want to weave at nettle_level 0
+                                        .weave(parent_code, child_code, std::cmp::min(1, level))
+                                        .await
+                                        .unwrap();
+                                    new_prompts.push(comp);
+                                }
+                            }
+
+                            prompts = new_prompts;
+                            debug!("number of level prompts: {}", prompts.len());
+                        }
+                    }
+                    match level.cmp(&0) {
+                        Ordering::Greater => {
+                            let ls = codex.get_ls();
+                            let mut new_comps = HashSet::new(); // we don't care about duplicates
+                            for prompt in prompts.iter() {
+                                let stubbed = ls.stub(prompt).await.unwrap();
+                                let mut printed =
+                                    ls.pretty_print(&stubbed, "_hole_").await.unwrap();
+
+                                // we add usages to the prompt
+                                if !node.usages.is_empty() {
+                                    printed = format!("{}\n{}", printed, node.usages);
+                                }
+
+                                let q = CompletionQuery::new(printed, num_comps, retries, false);
+
+                                debug!("query: {}", q.input);
+                                let comps = retry_query_until_ok(&codex, q).await;
+                                for comp in comps {
+                                    debug!("level comp: \n{}", comp.code);
+                                    let rewoven = ls.weave(prompt, &comp.code, 0).await.unwrap();
+                                    new_comps.insert(rewoven);
+                                }
+                            }
+                            (node.name, new_comps.into_iter().collect())
+                        }
+                        // if we are at root, we just want to disassemble the tree, no comps
+                        Ordering::Equal => (node.name, prompts),
+                        Ordering::Less => unreachable!(),
+                    }
+                }));
+            }
+            for handle in handles {
+                let (name, comps) = handle.await.unwrap();
+                let idx = lookup.get(&name).unwrap();
+                nodes.get_mut(*idx).unwrap().completed = comps;
+            }
+            debug!("setting prev_level");
+            prev_level = Arc::new(Some(nodes.clone()));
+        }
+    }
+}
