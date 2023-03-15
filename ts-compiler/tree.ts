870dbf707050de05c4105e6898dd0f9c18b11e51
elleven11 <federico.cassano@federico.codes>
2023-03-15 14:42:09 -0400

    toplevel tree fixes

--- ts-compiler/tree.ts
+++ ts-compiler/tree.ts
@@ -7,6 +7,30 @@ type CodeBlockTree = {
   children: CodeBlockTree[];
 };
 
+// returns true if the given node can include a type annotation
+// this is not exhaustive, only used for makeTree edge cases
+const isTypable = (node: ts.Node) => {
+  const isFuncAndHasBody = (node: ts.Node) => {
+    if (
+      node.kind === ts.SyntaxKind.FunctionExpression ||
+      node.kind === ts.SyntaxKind.ArrowFunction
+    ) {
+      const func = node as ts.FunctionExpression;
+      if (func.body.kind === ts.SyntaxKind.Block) {
+        return true;
+      }
+
+      return false;
+    }
+  };
+  return (
+    (ts.isVariableDeclaration(node) &&
+      !(node.initializer && isFuncAndHasBody(node.initializer))) ||
+    ts.isInterfaceDeclaration(node) ||
+    ts.isFunctionLike(node)
+  );
+};
+
 export const makeTree = (sourceFile: ts.SourceFile): CodeBlockTree => {
   const usedRandomNumbers = new Set();
   const symgen = (prefix: string) => {
@@ -18,71 +42,81 @@ export const makeTree = (sourceFile: ts.SourceFile): CodeBlockTree => {
     return prefix + "$" + n;
   };
 
-  const traverse = (child: ts.Node, ctxParentNode: CodeBlockTree): void => {
-    const code = codePrinter.printNode(
-      ts.EmitHint.Unspecified,
-      child,
-      sourceFile
-    );
-
-    // function declarations
-    if (child.kind === ts.SyntaxKind.FunctionDeclaration) {
-      const func = child as ts.FunctionDeclaration;
-      // idk why, but sometimes a func doesn't have a name, but it's not necessarily an anonymous function
-      if (func.name) {
-        const name = symgen(func.name.escapedText.toString());
-        let thisNode = { name, code, children: [] };
-
-        func.body?.statements.forEach((child) => traverse(child, thisNode));
-
-        ctxParentNode.children.push(thisNode);
-
-        return;
-      }
+  const traverse = (
+    child: ts.Node,
+    ctxParentNode: CodeBlockTree,
+    atTop = false
+  ) => {
+    // skip FirstStatement, EndOfFileToken
+    // we want the children of these nodes to simplify the search
+    if (
+      child.kind === ts.SyntaxKind.FirstStatement ||
+      child.kind === ts.SyntaxKind.EndOfFileToken ||
+      child.kind === ts.SyntaxKind.VariableDeclarationList
+    ) {
+      child.forEachChild((child) => traverse(child, ctxParentNode, atTop));
+      return;
     }
 
-    // console.log("kind: ", child.kind);
-    // console.log("code: ", code);
+    const generateCode = (node: ts.Node) => {
+      const code = codePrinter.printNode(
+        ts.EmitHint.Unspecified,
+        node,
+        sourceFile
+      );
+      return code;
+    };
 
-    // arrow funcs / anonymous functions (that are let-bound)
-    if (
-      child.kind === ts.SyntaxKind.FunctionExpression ||
-      child.kind === ts.SyntaxKind.ArrowFunction
+    // function declarations
+    if (ts.isFunctionDeclaration(child) && child.name) {
+      // idk why, but sometimes a func doesn't have a name, but it's not necessarily an anonymous function
+      const name = symgen(child.name.escapedText.toString());
+      let thisNode = { name, code: generateCode(child), children: [] };
+
+      child.body?.statements.forEach((child) => traverse(child, thisNode));
+
+      ctxParentNode.children.push(thisNode);
+
+      return;
+    } else if (
+      (ts.isFunctionExpression(child) || ts.isArrowFunction(child)) &&
+      child.parent &&
+      ts.isVariableDeclaration(child.parent)
     ) {
       // check if parent is a variable declaration
       const func = child as ts.FunctionExpression;
-      if (child.parent?.kind === ts.SyntaxKind.VariableDeclaration) {
-        const varDec = child.parent as ts.VariableDeclaration;
-        const code = codePrinter.printNode(
-          ts.EmitHint.Unspecified,
-          varDec.parent, // go another level up
-          sourceFile
-        );
-        const nameId = varDec.name as ts.Identifier;
-        const name = symgen(nameId.escapedText.toString());
-        let thisNode = { name, code, children: [] };
+      const varDec = child.parent as ts.VariableDeclaration;
+      const code = generateCode(varDec.parent);
+      const nameId = varDec.name as ts.Identifier;
+      const name = symgen(nameId.escapedText.toString());
+      let thisNode = { name, code, children: [] };
 
-        if (func.body.statements) {
-          func.body?.statements.forEach((child) => traverse(child, thisNode));
-          ctxParentNode.children.push(thisNode);
-          return;
-        }
-      }
-    }
-
-    // classes
-    if (child.kind === ts.SyntaxKind.ClassDeclaration) {
-      const classDec = child as ts.ClassDeclaration;
-      if (classDec.name) {
-        const name = symgen(classDec.name.escapedText.toString());
-        let thisNode = { name, code, children: [] };
-
-        classDec.members.forEach((child) => traverse(child, thisNode));
-
+      if (func.body.statements) {
+        func.body?.statements.forEach((child) => traverse(child, thisNode));
         ctxParentNode.children.push(thisNode);
-
         return;
       }
+    } else if (ts.isClassDeclaration(child) && child.name) {
+      const name = symgen(child.name.escapedText.toString());
+      let thisNode = { name, code: generateCode(child), children: [] };
+
+      child.members.forEach((child) => traverse(child, thisNode));
+
+      ctxParentNode.children.push(thisNode);
+
+      return;
+    } else if (atTop && isTypable(child)) {
+      console.log("top level", ts.SyntaxKind[child.kind]);
+      // toplevel node, make a child
+      const name = symgen("topnode");
+      let code;
+      if (ts.isVariableDeclaration(child)) {
+        code = generateCode(child.parent);
+      } else {
+        code = generateCode(child);
+      }
+      let thisNode = { name, code, children: [] };
+      ctxParentNode.children.push(thisNode);
     }
 
     child.forEachChild((child) => {
@@ -100,7 +134,7 @@ export const makeTree = (sourceFile: ts.SourceFile): CodeBlockTree => {
   };
 
   ts.forEachChild(sourceFile, (child) => {
-    traverse(child, tree);
+    traverse(child, tree, true);
   });
 
   return tree;
