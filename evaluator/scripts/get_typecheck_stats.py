46bcd501ac53a09a8dde607fe1fa68fcf9af15fc
elleven11 <federico.cassano@federico.codes>
2023-04-18 19:44:27 -0400

    updated script

--- evaluator/scripts/get_typecheck_stats.py
+++ evaluator/scripts/get_typecheck_stats.py
@@ -4,19 +4,29 @@ from utils import read_jsonl
 def get_num_typecheck(data_path):
     num_elems = 0
     num_typecheck = 0
+    num_dont_typecheck = 0
+    avg_errors = 0
     avg_heuristic = 0
     for elem in read_jsonl(data_path):
         num_elems += 1
+        had_one_typecheck = False
         for comp in elem["completions"]:
-            if comp["does_typecheck"]:
+            num_errors = comp["num_type_errors"]
+            if num_errors == 0 and not had_one_typecheck:
                 num_typecheck += 1
-                avg_heuristic += comp["heuristic"]
-                break
+                avg_heuristic += comp["score"]
+                had_one_typecheck = True
+            elif num_errors > 0:
+                num_dont_typecheck += 1
+                avg_errors += num_errors
 
     avg_heuristic /= num_typecheck
+    avg_errors /= num_dont_typecheck
 
     print("Number of elements: {}".format(num_elems))
     print("Number of elements that typecheck: {}".format(num_typecheck))
+    print("Number of elements that don't typecheck: {}".format(num_dont_typecheck))
+    print("Average number of errors in the ones that don't typecheck: {}".format(avg_errors))
     print("Average heuristic (lower is better): {}".format(avg_heuristic))
 
 if __name__ == "__main__":
