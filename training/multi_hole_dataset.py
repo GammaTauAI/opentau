8bf3f2c486227b2190c7022e408b06ad21d13a3f
cassanof <federico.cassano@federico.codes>
2023-12-09 21:23:28 -0500

    multi

--- training/multi_hole_dataset.py
+++ training/multi_hole_dataset.py
@@ -1,7 +1,19 @@
 import datasets
 from fim import permute_multi_holes
+import os
 import numpy as np
 
 base_ds = datasets.load_dataset("nuprl/ts-training", split="train", revision="v1.1p1")
 rng = np.random.default_rng(42)
+max_holes = 6
+min_holes = 1
 
+def process(ex):
+    num_holes = rng.integers(min_holes, max_holes + 1)
+    content = ex["content"]
+    new = permute_multi_holes(content, rng, num_holes)
+    ex["content"] = new
+    return {"original_content": content,  **ex}
+
+ds = base_ds.map(process)
+ds.push_to_hub("nuprl/ts-multi-hole-training")
